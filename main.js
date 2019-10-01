190b1d690e577c12d6d3360cffbe2c559be83026
Sung Paik <sung.paik@codeimmersives.com>
2019-10-01 15:35:21 -0400

    my solution

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function fizzy(num) {
   if(num % 3 === 0 && num % 5 === 0) {
     return 'FizzBuzz';
-  } else if(num % 3 === 0 && !(num % 5 === 0)) {
+  } else if(num % 3 === 0) {
     return 'Fizz';
-  } else if(num % 5 === 0 && !(num % 3 === 0)) {
+  } else if(num % 5 === 0) {
     return 'Buzz';
   } else {
     return num;
