4fa34f253cff0092da6fc39f9e11a1363af6d050
Sung Paik <sung.paik@codeimmersives.com>
2019-10-01 15:12:40 -0400

    solution

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function fizzy(num) {
-
+  if(num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  } else if(num % 3 === 0) {
+    return 'Fizz';
+  } else if(num % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return num;
+  }
 }
+
+console.log(fizzy(1));
 
 module.exports = {
   fizzy,
